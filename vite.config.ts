63f4330e05d26a76d134056c41007d217f7fde57
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-03-25 17:34:56 +0100

    done - todo : render

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+///*
+
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/interview_react_live_filter_search/'
+  }
+
+  return config
+})
+
+//*/
+
+/*
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+
+*/
